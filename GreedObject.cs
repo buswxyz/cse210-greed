59b0748e01ea21c38a68a7f0c08472564fa13f2e
Stryker-Blade <jensen.d.call@gmail.com>
2022-11-05 13:00:43 -0600

    Added functionality
    Add velocity for falling objects, starting positions, lists to track objects, and the functionality for the program to continously draw the objects (so that they appear to move.)

---
+++ GreedObject.cs
@@ -0,0 +1,75 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace Helloworld{
+class Colors{
+    protected List<string> colorNames = new List<string>();
+    protected List<string> createColorList(List<string> colorNames){
+        colorNames.Add("Red");
+        colorNames.Add("Orange");
+        colorNames.Add("Yellow");
+        colorNames.Add("Green");
+        colorNames.Add("Blue");
+        colorNames.Add("Purple");
+        colorNames.Add("White");
+        
+        return colorNames;
+    }
+}
+
+
+
+class FallingObject: Color{
+    public Vector2 Position { get; set; } = new Vector2(0, 0);
+    public Vector2 Velocity { get; set; } = new Vector2(0, 0);
+
+
+    virtual public void Draw(){
+
+    }
+    public void Move() {
+        Vector2 NewPosition = this.Position;
+        NewPosition.X += Velocity.X;
+        NewPosition.Y += Velocity.Y;
+        this.Position = NewPosition;
+    }
+    }
+
+class Gem: FallingObject{
+
+    public int Size {get; set;} 
+
+    public Gem(Color color, int size): base(color) {
+        Size = size;
+    }
+    // Add a random to make the color chosen random
+    override public void Draw() {
+        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, Size, Size, Color[0]);
+    }
+}
+
+class Rock: FallingObject{
+    public int Radius { get; set; }
+
+    public Rock(Color color, int radius): base(color) {
+        Radius = radius;
+    }
+     // Add a random to make the color chosen random
+    override public void Draw() {
+        Raylib.DrawCircle((int)Position.X, (int)Position.Y, Radius, Color[1]);
+    }
+
+}
+
+class Player{
+
+}
+
+class CheckCollision{
+
+}
+
+class Score{
+
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,61 +1,75 @@
-﻿using Raylib_cs;
+using Raylib_cs;
+using System.Numerics;
 
-class Color{
-    protected List<string> colorNames = new List<string>();
-    protected List<string> createColorList(List<string> colorNames){
-        colorNames.Add("Red");
-        colorNames.Add("Orange");
-        colorNames.Add("Yellow");
-        colorNames.Add("Green");
-        colorNames.Add("Blue");
-        colorNames.Add("Purple");
-        return colorNames;
-    }
-}
+namespace Helloworld{
+static class Program{
+ public static void Main()
+        {
 
-class Location: Color{
-    Random random = new Random();
-    protected int y = 400;
-    protected int getRandomX(){
-        int x = random.Next(20, 780);
-        return x;
-    }
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+            var Objects = new List<FallingObject>();
+            var Random = new Random();
 
-}
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "Greed");
+            Raylib.SetTargetFPS(60);
 
-class FallingObject: Location{
-    Random random = new Random();
-    protected int movementSpeed = random.Next(1, 8);
-    protected void fallingSpeed(){
+            while (!Raylib.WindowShouldClose())
+            {
+                // Add a new random object to the screen every iteration of our game loop
+                var whichType = Random.Next(3);
 
-    }
-}
+                // Generate a random velocity for this object
+                var randomY = Random.Next(1, 4);
+                var randomX = Random.Next(1, 4);
+                
+                //Generating a random x coordinate for each object
+                var xposition = Random.Next(20,780);
+                
+                // I am generating the positions of the falling objects here
+                var position = new Vector2(xposition, 460);
+            
+                switch (whichType) {
+                            case 0:
+                            //Tempararoly using squares for gem, need to switch to actual drawing
+                                Console.WriteLine("Creating a gem");
+                                var gem = new Gem(Colors[1], 50);
+                                gem.Position = position;
+                                gem.Velocity = new Vector2(randomX, randomY);
+                                Objects.Add(square);
+                                break;
+                        case 1:
+                        //Tempararoly using circles for rock, need to switch to actual drawing
+                            Console.WriteLine("Creating a rock");
+                            var rock = new Rock(Colors[2], 25);
+                            rock.Position = position;
+                            rock.Velocity = new Vector2(randomX, randomY);
+                            Objects.Add(rock);
+                            break;
+                        default:
+                            break;
+                        
+                        }
+                    Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Colors[6]);
 
-class Rock: FallingObject{
+                    // Draw all of the objects in their current location
+                    foreach (var obj in Objects) {
+                        obj.Draw();
+                    }
 
-}
+                    
 
-class Gem: FallingObject{
+                    Raylib.EndDrawing();
 
-}
+                    // Move all of the objects to their next location
+                    foreach (var obj in Objects) {
+                        obj.Move();
+                    }
+            }
 
-class Player{
-
-}
-
-class CheckCollision{
-
-}
-
-class Score{
-
-}
-
-class Window{
-    public void createWindow(){
-        var windowHeight = 480;
-        var windowWidth = 800;
-        Raylib.InitWindow(windowWidth, windowHeight, "Greed");
-        Raylib.SetTargetFPS(60);
-    }
-}
+            Raylib.CloseWindow();
+        }
+            
+}  
+} 
